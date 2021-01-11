aee980c97fc249e0a07dce96a70efa8982d22933
Arragh <arragh@mail.ru>
2021-01-11 16:42:30 +0300

    Добавьте файлы проекта.

---
+++ UrlsAndRoutes2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes2.Models;
+
+namespace UrlsAndRoutes2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(nameof(Result), new Result { Controller = nameof(HomeController), Action = nameof(Index) });
+
+        public ViewResult CustomVariable(string id)
+        {
+            Result result = new Result { Controller = nameof(HomeController), Action = nameof(CustomVariable) };
+            result.Data["id"] = id ?? "<не задано>";
+            return View(nameof(Result), result);
+        }
+    }
+}
---
+++ UrlsAndRoutes2/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlsAndRoutes2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(null, "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
